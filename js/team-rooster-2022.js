67c96af082e741615bbd4444e361e2a1ca290338
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-03-01 14:23:48 -0600

    Added  Hardlus members

--- js/team-rooster-2022.js
+++ js/team-rooster-2022.js
@@ -1,6 +1,13 @@
 const load2022TeamRooster = function(){
     const students = [
-      new Student("Fernando", "fernando.jpg", [TeamAreas.MESA])
+      new Student("Celeste", "celeste.jpg", [TeamAreas.FINANZAS, TeamAreas.SOCIALES, TeamAreas.INCLUSION], "Cel"),
+      new Student("Arturo", "arturo.jpg", [TeamAreas.INGENIERIA, TeamAreas.FINANZAS, TeamAreas.SOCIALES]),
+      new Student("Hiroki", "hiroki.jpg", [TeamAreas.FINANZAS, TeamAreas.SOCIALES, TeamAreas.INCLUSION], "Hiro"),
+      new Student("Natalia", "nat.jpg", [TeamAreas.INGENIERIA, TeamAreas.SOCIALES, TeamAreas.INCLUSION], "Nat"),
+      new Student("Sarah", "sarah.jpg", [TeamAreas.FINANZAS, TeamAreas.SOCIALES, TeamAreas.COMUNICACION]),
+      new Student("Marian", "marian.jpg", [TeamAreas.INGENIERIA, TeamAreas.SOCIALES]),
+      new Student("Estefanía", "estef.jpg", [TeamAreas.INGENIERIA, TeamAreas.FINANZAS, TeamAreas.SOCIALES], "Estef"),
+      new Student("Paola", "paola.jpg", [TeamAreas.INGENIERIA, TeamAreas.SOCIALES], "Pao")
     ];
   
     const mentors = [
@@ -8,7 +15,8 @@ const load2022TeamRooster = function(){
     ];
   
     const coaches = [
-      new Coach("Cinthya", "cinthya.jpg", [TeamAreas.HEAD_COACH], "Cin")
+      new Coach("Víctor", "vic.jpg", [TeamAreas.HEAD_COACH], "Vic"),
+      new Coach("Manuel", "manu.jpg", [TeamAreas.COACH], "Manu")
     ];
   
     const tr = new TeamRooster(students, mentors, coaches, 2022);
