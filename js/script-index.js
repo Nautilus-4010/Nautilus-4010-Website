a40ffb7fd9208cddba45e4b96c5d9ff0a36e520c
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-03-02 17:34:36 -0600

    Moved galería to own page

--- js/script-index.js
+++ js/script-index.js
@@ -3,14 +3,6 @@ const main = function() {
     transparentarNav();
   });
   document.querySelector('nav').classList.add("sin-fondo");
-  const slider = new MainSlider(".galeria-fotos");
-  slider.renderContent();
-  setInterval(() => {
-    slider.changeImage();
-  }, slider.DELAY_FOR_IMAGE_CHANGE);
-  window.onresize = function() {
-    slider.renderContent();
-  }
 }
 
 function transparentarNav() {
