4229a2f66912ac38f54facfb87db6eeef6d0c0fa
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-03-07 01:23:49 -0600

    Orden alfabético

--- js/team-rooster-2022.js
+++ js/team-rooster-2022.js
@@ -1,13 +1,13 @@
 const load2022TeamRooster = function(){
     const students = [
-      new Student("Celeste", "celeste.jpg", [TeamAreas.FINANZAS, TeamAreas.SOCIALES, TeamAreas.INCLUSION], "Cel"),
       new Student("Arturo", "arturo.jpg", [TeamAreas.INGENIERIA, TeamAreas.FINANZAS, TeamAreas.SOCIALES]),
-      new Student("Hiroki", "hiroki.jpg", [TeamAreas.FINANZAS, TeamAreas.SOCIALES, TeamAreas.INCLUSION], "Hiro"),
-      new Student("Natalia", "nat.jpg", [TeamAreas.INGENIERIA, TeamAreas.SOCIALES, TeamAreas.INCLUSION], "Nat"),
-      new Student("Sarah", "sarah.jpg", [TeamAreas.FINANZAS, TeamAreas.SOCIALES, TeamAreas.COMUNICACION]),
-      new Student("Marian", "marian.jpg", [TeamAreas.INGENIERIA, TeamAreas.SOCIALES]),
+      new Student("Celeste", "celeste.jpg", [TeamAreas.FINANZAS, TeamAreas.SOCIALES, TeamAreas.INCLUSION], "Cel"),
       new Student("Estefanía", "estef.jpg", [TeamAreas.INGENIERIA, TeamAreas.FINANZAS, TeamAreas.SOCIALES], "Estef"),
-      new Student("Paola", "paola.jpg", [TeamAreas.INGENIERIA, TeamAreas.SOCIALES], "Pao")
+      new Student("Hiroki", "hiroki.jpg", [TeamAreas.FINANZAS, TeamAreas.SOCIALES, TeamAreas.INCLUSION], "Hiro"),
+      new Student("Marian", "marian.jpg", [TeamAreas.INGENIERIA, TeamAreas.SOCIALES]),
+      new Student("Natalia", "nat.jpg", [TeamAreas.INGENIERIA, TeamAreas.SOCIALES, TeamAreas.INCLUSION], "Nat"),
+      new Student("Paola", "paola.jpg", [TeamAreas.INGENIERIA, TeamAreas.SOCIALES], "Pao"),
+      new Student("Sarah", "sarah.jpg", [TeamAreas.FINANZAS, TeamAreas.SOCIALES, TeamAreas.COMUNICACION])
     ];
   
     const mentors = [
