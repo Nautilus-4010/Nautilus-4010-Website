e9377eeb0b33da5b37859c5ba6b4aabf70dac299
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-03-07 01:42:18 -0600

    Added more members

--- js/team-rooster-2022.js
+++ js/team-rooster-2022.js
@@ -1,17 +1,32 @@
 const load2022TeamRooster = function(){
     const students = [
+      new Student("Alejandra", "ale.jpg", [TeamAreas.COMUNICACION]),
+      new Student("Alexis", "alexis.jpg", [TeamAreas.INGENIERIA, TeamAreas.FINANZAS, TeamAreas.SOCIALES]),
+      new Student("Ana", "ana.jpg", [TeamAreas.INGENIERIA]),
       new Student("Arturo", "arturo.jpg", [TeamAreas.INGENIERIA, TeamAreas.FINANZAS, TeamAreas.SOCIALES]),
+      new Student("Camila", "camila.jpg", [TeamAreas.INGENIERIA, TeamAreas.FINANZAS, TeamAreas.SOCIALES], "Mila"),
       new Student("Celeste", "celeste.jpg", [TeamAreas.FINANZAS, TeamAreas.SOCIALES, TeamAreas.INCLUSION], "Cel"),
+      new Student("Cinthya", "cinthya.jpg", [TeamAreas.INGENIERIA, TeamAreas.SOCIALES], "Cin"),
+      new Student("David", "david.jpg", [TeamAreas.INGENIERIA, TeamAreas.FINANZAS, TeamAreas.SOCIALES]),
+      new Student("Emma", "emma.jpg", [TeamAreas.INCLUSION]),
       new Student("Estefanía", "estef.jpg", [TeamAreas.INGENIERIA, TeamAreas.FINANZAS, TeamAreas.SOCIALES], "Estef"),
+      new Student("Fernando", "fernando.jpg", [TeamAreas.INGENIERIA, TeamAreas.FINANZAS, TeamAreas.SOCIALES], "Fer"),
       new Student("Hiroki", "hiroki.jpg", [TeamAreas.FINANZAS, TeamAreas.SOCIALES, TeamAreas.INCLUSION], "Hiro"),
+      new Student("Iñaki", "iñaki.jpg", [TeamAreas.INGENIERIA, TeamAreas.FINANZAS, TeamAreas.SOCIALES]),
       new Student("Marian", "marian.jpg", [TeamAreas.INGENIERIA, TeamAreas.SOCIALES]),
       new Student("Natalia", "nat.jpg", [TeamAreas.INGENIERIA, TeamAreas.SOCIALES, TeamAreas.INCLUSION], "Nat"),
       new Student("Paola", "paola.jpg", [TeamAreas.INGENIERIA, TeamAreas.SOCIALES], "Pao"),
-      new Student("Sarah", "sarah.jpg", [TeamAreas.FINANZAS, TeamAreas.SOCIALES, TeamAreas.COMUNICACION])
+      new Student("Sarah", "sarah.jpg", [TeamAreas.FINANZAS, TeamAreas.SOCIALES, TeamAreas.COMUNICACION]),
+      new Student("Ulises", "ulises.jpg", [TeamAreas.INGENIERIA, TeamAreas.FINANZAS, TeamAreas.SOCIALES], "Uli"),
+      new Student("Valeria", "vale.jpg", [TeamAreas.INGENIERIA, TeamAreas.FINANZAS, TeamAreas.SOCIALES], "Vale")
     ];
   
     const mentors = [
-      new Mentor("Diego", "dieguito.jpg", [TeamAreas.INGENIERIA, TeamAreas.SOCIALES])
+      new Mentor("Alejandro", "alex.jpg", [TeamAreas.INGENIERIA, TeamAreas.SOCIALES], "Alex"),
+      new Mentor("Diego", "dieguito.jpg", [TeamAreas.INGENIERIA, TeamAreas.SOCIALES], "Dieguito"),
+      new Mentor("Jorge", "jorge.jpg", [TeamAreas.INGENIERIA, TeamAreas.SOCIALES, TeamAreas.FINANZAS], "Jorgito"),
+      new Mentor("Rodrigo", "rodrigo.jpg", [TeamAreas.INGENIERIA], "Ro"),
+      new Mentor("Santiago", "santi.jpg", [TeamAreas.INGENIERIA, TeamAreas.COMUNICACION], "Santi")
     ];
   
     const coaches = [
